a81f086953689fb8728a6d31561af913915ca08f
Alan chen <alanchen8647@gmail.com>
2025-09-29 19:21:59 -0400

    team-eyja

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import jPhoto from "./assets/j.jpg";
+import yPhoto from "./assets/y.png";
+import aPhoto from "./assets/a.jpg";
+import ePhoto from "./assets/e.png";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
     
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
+            <h1>Project Team - EYJA</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
+                    <img src={jPhoto} className="logo react" alt="React logo" />
+                    <img src={yPhoto} className="logo react" alt="React logo" />
+                    <img src={aPhoto} className="logo react" alt="React logo" />
+                    <img src={ePhoto} className="logo react" alt="React logo" />
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+
         </>
     );
 }
