0c65aca36b5be5309b33a49011ba02196cfbc75a
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-09-30 08:44:02 -0400

    Add subtitles and borders to two sections.

--- src/groupMemberPage/emmanuel.jsx
+++ src/groupMemberPage/emmanuel.jsx
@@ -5,28 +5,37 @@ import "./emmanuel.css";
 function Emmanuel() {
     return (
         <>
-            <div className="profileContainer">
-                <img className="profilePic" src={ePhoto}></img>
+            <div className="emmanuelPage">
+                <div className="profileContainer">
+                    <img className="profilePic" src={ePhoto}></img>
+                </div>
+                <div className="infoContainer">
+                    <h1 className="heading">Emmanuel Oyelade</h1>
+                    <div className="subtitlesContainer">
+                        <h2 className="subtitle">Computer Science</h2>
+                        <h2>&bull;</h2>
+                        <h2 className="subtitle">Senior</h2>
+                        <h2>&bull;</h2>
+                        <h2 className="subtitle">Baruch</h2>
+                    </div>
+                    <div className="backgroundContainer">
+                        <h2>My Background</h2>
+                        <p>I originally came to Baruch as an accounting major, because it seemed like a major that I was capable 
+                            of doing. However, after seeing peoples programming projects online and researching what one can do
+                            with programming, I fell in love with it. So, I quickly changed my major from accounting to computer 
+                            science as soon as it became an official major. Now, I am a computer science senior that has taken
+                            various classes in math and cis in with the sole purpose of picking up as many programming skills as 
+                            possible while I am still in Baruch.</p>
+                    </div>
+                    <div>
+                        <h2>Skills</h2>
+                    </div>
+                    <div>
+                        <h2>What I Want to Learn</h2>
+                    </div>
+                </div>
             </div>
-            <div className="infoContainer">
-                <h2>Emmanuel's Page</h2>
-                <p>Hi! I am Emmanuel. I am a senior majoring in Computer Science. Looking forward to working with you!</p>
-                <div>
-                    <h2>My Background</h2>
-                    <p>I originally came to Baruch as an accounting major, because it seemed like a major that I was capable 
-                        of doing. However, after seeing peoples programming projects online and researching what one can do
-                        with programming, I fell in love with it. So, I quickly changed my major from accounting to computer 
-                        science as soon as it became an official major. Now, I am a computer science senior that has taken
-                        various classes in math and cis in with the sole purpose of picking up as many programming skills as 
-                        possible while I am still in Baruch.</p>
-                </div>
-                <div>
-                    <h2>Skills</h2>
-                </div>
-                <div>
-                    <h2>What I Want to Learn</h2>
-                </div>
-            </div>
+            
         </>
     );
 }
