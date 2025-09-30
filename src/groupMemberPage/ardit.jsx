cebcaf3cea56658bb1d1575383591a0de0e221c5
Ardit Rexha <ardit.rexha@baruchmail.cuny.edu>
2025-09-29 20:39:29 -0400

    how are you

--- src/groupMemberPage/ardit.jsx
+++ src/groupMemberPage/ardit.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Ardit() {
 
     return (
-        
+    
         <div>
-            <h1>How are you?</h1>
+            <h1>How are youu?</h1>
             <h2>Ardit's Page</h2>
             <p>Hi! I am Ardit. I am a Junior majoring in Computer Science. Looking forward to working with you!</p>
         </div>
