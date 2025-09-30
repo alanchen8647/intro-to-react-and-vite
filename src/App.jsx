195adf1a7cd3e63ff4fe771d903fb8bd1540d4c7
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-09-29 20:24:29 -0400

    Add image on Emmanuel's page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import jPhoto from "./assets/j.jpg";
 import yPhoto from "./assets/y.png";
 import aPhoto from "./assets/a.jpg";
-import ePhoto from "./assets/e.png";
+import ePhoto from "./assets/e.jpg";
 import { useState , useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
@@ -12,7 +12,7 @@ function App() {
         "Jianping": "Hi! I am Jianping, also go by Alan. Currently a senior majoring in Computer Science. Hope to know you more!",
         "Yiming": "Hi! I am Yujin Kim. I am a senior majoring in Computer Science. Nice to meet you!",
         "Ardit": "Hi! I am Alan Chen. I am a senior majoring in Computer Science. Looking forward to working with you!",
-        "Emmanuel": "Hi! I am Eunice Lee. I am a senior majoring in Computer Science. Excited to collaborate!",
+        "Emmanuel": "Hi! I am Emmanuel Oyelade. I am a senior majoring in Computer Science. Excited to collaborate!",
         "default": "Click on our avatar to know more about us!"
     }
 
@@ -25,7 +25,7 @@ function App() {
                 <a onClick={e=>setIntro("Jianping")}><img src={jPhoto} className="logo react" alt="Jianping's photo" /></a>
                 <a onClick={e=>setIntro("Yiming")}><img src={yPhoto} className="logo react" alt="Yiming's photo" /></a>
                 <a onClick={e=>setIntro("Ardit")}><img src={aPhoto} className="logo react" alt="Ardit's photo" /></a>
-                <a onClick={e=>setIntro("Emmanuel")}><img src={ePhoto} className="logo react" alt="Emmanuel's photo" /></a>
+                <a onClick={e=>setIntro("Emmanuel")}><img src={ePhoto} className="logo react" alt="Emmanuel's photo" width="100" height="110"/></a>
             </div>
             <div className="card">
                 <p>{groupIntroduction[intro]}</p>
--- src/groupMemberPage/emmanuel.jsx
+++ src/groupMemberPage/emmanuel.jsx
@@ -1,11 +1,17 @@
 import react from 'react';
+import ePhoto from "../assets/e.jpg"; 
 
 function Emmanuel() {
     return (
-        <div>
-            <h2>Emmanuel's Page</h2>
-            <p>Hi! I am Emmanuel. I am a senior majoring in Computer Science. Looking forward to working with you!</p>
-        </div>
+        <>
+            <div>
+                <img className="profilePic" src={ePhoto}></img>
+            </div>
+            <div>
+                <h2>Emmanuel's Page</h2>
+                <p>Hi! I am Emmanuel. I am a senior majoring in Computer Science. Looking forward to working with you!</p>
+            </div>
+        </>
     );
 }
 
